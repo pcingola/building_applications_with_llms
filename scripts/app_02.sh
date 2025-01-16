e54ba3b3cf39e5d71137b0fed82483e2ed1b3656
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2025-01-16 14:43:17 -0500

    Project updated

---
+++ scripts/app_02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+set -o pipefail
+
+# Get script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+PROJECT_DIR="$(cd $SCRIPT_DIR/.. && pwd -P)"
+
+# Activate virtual environment
+echo "PROJECT_DIR: $PROJECT_DIR"
+cd $PROJECT_DIR
+source .venv/bin/activate
+
+# Run the ingest_transcripts.py script
+chainlit run app_02.py -w
